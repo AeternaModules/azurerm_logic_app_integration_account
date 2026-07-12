81ebfcc2fbd69ac29ce994234c1ae15c570be00e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "logic_app_integration_accounts" {
-  description = "All logic_app_integration_account resources"
-  value       = azurerm_logic_app_integration_account.logic_app_integration_accounts
-}
 output "logic_app_integration_accounts_integration_service_environment_id" {
-  description = "List of integration_service_environment_id values across all logic_app_integration_accounts"
-  value       = [for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : v.integration_service_environment_id]
+  description = "Map of integration_service_environment_id values across all logic_app_integration_accounts, keyed the same as var.logic_app_integration_accounts"
+  value       = { for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : k => v.integration_service_environment_id }
 }
 output "logic_app_integration_accounts_location" {
-  description = "List of location values across all logic_app_integration_accounts"
-  value       = [for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : v.location]
+  description = "Map of location values across all logic_app_integration_accounts, keyed the same as var.logic_app_integration_accounts"
+  value       = { for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : k => v.location }
 }
 output "logic_app_integration_accounts_name" {
-  description = "List of name values across all logic_app_integration_accounts"
-  value       = [for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : v.name]
+  description = "Map of name values across all logic_app_integration_accounts, keyed the same as var.logic_app_integration_accounts"
+  value       = { for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : k => v.name }
 }
 output "logic_app_integration_accounts_resource_group_name" {
-  description = "List of resource_group_name values across all logic_app_integration_accounts"
-  value       = [for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : v.resource_group_name]
+  description = "Map of resource_group_name values across all logic_app_integration_accounts, keyed the same as var.logic_app_integration_accounts"
+  value       = { for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : k => v.resource_group_name }
 }
 output "logic_app_integration_accounts_sku_name" {
-  description = "List of sku_name values across all logic_app_integration_accounts"
-  value       = [for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : v.sku_name]
+  description = "Map of sku_name values across all logic_app_integration_accounts, keyed the same as var.logic_app_integration_accounts"
+  value       = { for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : k => v.sku_name }
 }
 output "logic_app_integration_accounts_tags" {
-  description = "List of tags values across all logic_app_integration_accounts"
-  value       = [for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : v.tags]
+  description = "Map of tags values across all logic_app_integration_accounts, keyed the same as var.logic_app_integration_accounts"
+  value       = { for k, v in azurerm_logic_app_integration_account.logic_app_integration_accounts : k => v.tags }
 }
 
